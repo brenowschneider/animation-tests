f47faf7aff1e1f36821f111632c92ef05e9beeee
Breno Wruck Schneider <breno.schneider@PRODEST.ES.GOV.BR.LOCAL>
2016-05-11 17:14:05 -0300

    added some validations to keyboard.js

--- assets/js/keyboard.js
+++ assets/js/keyboard.js
@@ -1,6 +1,7 @@
 //keyboard codes
 var LEFT_ARROW = 37;
 var RIGHT_ARROW = 39;
+var SPACE_BAR = 32;
 
 function Keyboard(element) {
     'use strict';
@@ -14,17 +15,34 @@ function Keyboard(element) {
     this.shotFunction = [];
     
     var keyboard = this;
-    element.addEventListener('keydown', function(event) {
+    element.addEventListener('keydown', function (event) {
         keyboard.pressedKeys[event.keyCode] = true;
+        
+        //This method should be triggered on the first keydown only
+        if (keyboard.shotFunction[event.keyCode] && !keyboard.shots[event.keyCode]) {
+            keyboard.shots[event.keyCode] = true;
+            keyboard.shotFunction[event.keyCode]();
+        }
+        
     });
-    element.addEventListener('keyup', function(event) {
+    element.addEventListener('keyup', function (event) {
         keyboard.pressedKeys[event.keyCode] = false;
+        keyboard.shots[event.keyCode] = false;
     });
+    
+    
+    
     
 }
 
 Keyboard.prototype = {
-    pressed: function(key) {
+    pressed: function (key) {
+        'use strict';
         return this.pressed[key];
+    },
+    
+    shoot: function (key, callback) {
+        'use strict';
+        this.shotFunction[key] = callback;
     }
-}
+};
